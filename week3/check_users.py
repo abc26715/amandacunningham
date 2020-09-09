dae229238877d064ced790a584e0b07f0344fa8c
abc26715 <56202328+abc26715@users.noreply.github.com>
2020-09-09 19:51:23 -0400

    Delete check_users.py

--- week3/check_users.py
+++
@@ -1,9 +0,0 @@
-current_users = ['jamie', 'adam', 'michael', 'amanda', 'Lyla']
-new_users = ['katelyn', 'Josh', 'ABBIE', 'ryan', 'Brian']
-current_users_lower = [user.lower() for user in current_users
-
-for new_user in new_users:
-  if new_user.lower() in current_users_lower:
-  print("Sorry" + new_user "username unavailable.")
-  else:
-  print("Awesome" + new_user + "username available.")
